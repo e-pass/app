615608dc1262e28cf4acd6ba349116ebe474ae9a
Kirill L <kiriil.lapshin2022@gmail.com>
2024-03-09 01:26:08 +0300

    Fix fonts warning

--- App.tsx
+++ App.tsx
@@ -4,22 +4,24 @@ import { Navigation } from "./src/app/Navigation";
 
 const App = (): ReactElement => {
     const [fontsLoaded] = useFonts({
-        "Inter": "@/assets/fonts/Inter.ttf",
-        "Inter-Semibold": "@/assets/fonts/Inter-Semibold.otf",
-        "Navigo-Medium": "@/assets/fonts/Navigo-Medium.ttf",
-        "NunitoSans": "@/assets/fonts/NunitoSans.ttf",
-        "NunitoSans-Bold": "@/assets/fonts/NunitoSans-Bold.ttf",
-        "NunitoSans-Regular": "@/assets/fonts/NunitoSans-Regular.ttf",
-        "NunitoSans-Semibold": "@/assets/fonts/NunitoSans-Semibold.ttf",
-        "Roboto-Medium": "@/assets/fonts/Roboto-Medium.ttf",
-        "SF-Pro-Display-Bold": "@/assets/fonts/SF-Pro-Display-Bold.otf",
-        "SF-Pro-Display-Regular": "@/assets/fonts/SF-Pro-Display-Regular.otf",
-        "SF-Pro-Display-Semibold": "@/assets/fonts/SF-Pro-Display-Semibold.otf",
-        "SFProText-Bold": "@/assets/fonts/SFProText-Bold.ttf",
-        "SFProText-Medium": "@/assets/fonts/SFProText-Medium.ttf",
-        "SFProText-Regular": "@/assets/fonts/SFProText-Regular.ttf",
-        "SFProText-Semibold": "@/assets/fonts/SFProText-Semibold.ttf",
+        "Inter": require("./assets/fonts/Inter.ttf"),
+        "Inter-Semibold": require("./assets/fonts/Inter-Semibold.otf"),
+        "Navigo-Medium": require("./assets/fonts/Navigo-Medium.ttf"),
+        "NunitoSans": require("./assets/fonts/NunitoSans.ttf"),
+        "NunitoSans-Bold": require("./assets/fonts/NunitoSans-Bold.ttf"),
+        "NunitoSans-Regular": require("./assets/fonts/NunitoSans-Regular.ttf"),
+        "NunitoSans-Semibold": require("./assets/fonts/NunitoSans-Semibold.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+        "SF-Pro-Display-Bold": require("./assets/fonts/SF-Pro-Display-Bold.otf"),
+        "SF-Pro-Display-Regular": require("./assets/fonts/SF-Pro-Display-Regular.otf"),
+        "SF-Pro-Display-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+        "SFProText-Bold": require("./assets/fonts/SFProText-Bold.ttf"),
+        "SFProText-Medium": require("./assets/fonts/SFProText-Medium.ttf"),
+        "SFProText-Regular": require("./assets/fonts/SFProText-Regular.ttf"),
+        "SFProText-Semibold": require("./assets/fonts/SFProText-Semibold.ttf"),
     });
+
+    if (!fontsLoaded) return <></>;
     return <Navigation />;
 };
 
